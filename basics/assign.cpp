92eb7ccfb4551988ade73881fc6218be28e6527e
ChandanaMandarapu <chandanamandarapu93@gmail.com>
2025-03-08 12:19:07 +0530

    assigning approach

---
+++ basics/assign.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    int teaBags;
+
+    cout << "enter the number of teaBags you have: ";
+
+    cin >> teaBags;
+
+    if(teaBags < 10) {
+        // teaBags = teaBags + 5 ;
+        teaBags += 5;
+    }
+
+    cout << "Your total Bags are : " << teaBags;
+
+    return 0;
+}
